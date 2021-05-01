4158b95ee91e759aa008918677d8067ee7f5a6a9
Yasemin <gonenyasemin@gmail.com>
2021-05-01 11:23:03 +0300

    commited

---
+++ python-assignment/Fibonacci Numbers.py
@@ -0,0 +1,6 @@
+#Task : Create a list consisting of Fibonacci numbers from 1 to 55 using control flow statements.
+
+#The desired output is like :
+
+#fibonacci →  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
--- python-assignment/Is it a Prime Number.py
+++ python-assignment/Is it a Prime Number.py
@@ -4,3 +4,18 @@
 
 #input →  19 ⇉ output : 19 is a prime number
 #input →  10 ⇉ output : 10 is not a prime number
+
+number = int(input("please enter a number: "))
+abc = 0
+
+
+for i in range(1, n+1) :
+   if not n % i :
+       abc += 1
+
+if (n == 0) or (n == 1) or (abc >= 3) :
+    print(n, "is not prime number")
+else:
+    print(n, "is a prime number")
+
+
