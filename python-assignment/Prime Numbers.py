bdd7274b0b62e11f6ed1100fbaa930c0d1a179c0
Yasemin <gonenyasemin@gmail.com>
2021-05-05 19:52:16 +0300

    prime number

--- python-assignment/Prime Numbers.py
+++ python-assignment/Prime Numbers.py
@@ -8,4 +8,21 @@
 #61, 67, 71, 73, 79, 83, 89, 97]
 
 
+not_prime = []
+list_prime = []
+number = input("enter a number:")
+if number.isdecimal() == False or type(number) == float or int(number) <= 1 :
+    print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
+elif int(number) > 1 :
+    for i in range(2, int(number)+1) :
+        for j in range(2, i) :
+            if i % j == 0 :
+                not_prime.append(i)
+                break
+        else :
+            list_prime.append(i)
+print(f"Prime number is : ", list_prime)
+print(f"Not prime number is :", not_prime)
+
+
 
