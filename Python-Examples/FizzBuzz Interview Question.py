7994d810ba1f6dd51aad77bac637601418fc557b
Yasemin <gonenyasemin@gmail.com>
2021-05-05 21:45:08 +0300

    fizzbuzz

--- Python-Examples/FizzBuzz Interview Question.py
+++ Python-Examples/FizzBuzz Interview Question.py
@@ -1,5 +1,6 @@
 # FizzBuzz Interview Question
 # Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+#Print numbers from 1 to 100 inclusively following these instructions:
 
 # If the number is a multiple of 3 the output should be "Fizz".
 # If the number given is a multiple of 5, the output should be "Buzz".
@@ -7,15 +8,17 @@
 # If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
 # The output should always be a string even if it is not a multiple of 3 or 5.
 
-number = int(input("Please enter a number: "))
 
-if number % 15 == 0 :
-    print("FizzBuzz")
-elif number % 5 == 0 :
-    print("Buzz")
-elif number % 3 == 0 :
-    print("Fizz")
-else :
-    print(number)
+list_word = []
+for i in range(100) :
+    if i % 15 == 0 :
+        list_word.append("FizzBuzz")
+    elif i % 5 == 0 :
+        list_word.append("Buzz")
+    elif i % 3 == 0 :
+        list_word.append("Fizz")
+    else :
+        list_word.append(i)
+print(list_word)
 
 
