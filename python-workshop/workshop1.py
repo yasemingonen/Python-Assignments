4538e4af90ce5c68e704f83c6bc6d5490dc66b6e
Yasemin <gonenyasemin@gmail.com>
2021-05-13 18:00:20 +0300

    workshop

---
+++ python-workshop/workshop1.py
@@ -0,0 +1,40 @@
+"""
+Coding Challenge -1 : Validate Combination of Brackets
+Purpose of the this coding challenge is to solve a combination problem using loops.
+
+Learning Outcomes
+At the end of the this coding challenge, students will be able to;
+
+understand the use of loops.
+solve the advanced and complicated problems.
+understand the importance of pattern recognition.
+get a better understanding in manipulating lists or strings.
+Problem Statement
+Write a function that given a string containing just the characters (, ), {, }, [ and ], determines if the input string is valid or not by using following rules.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+
+Open brackets must be closed in the correct order.
+
+Note that an empty string is also considered valid.
+
+Example for user inputs and respective outputs
+
+Input        Output
+--------:    ------:
+"()"         True
+"()[]{}"     True
+"(]"         False
+"([)]"       False
+"{[]}"       True
+""           True
+"""
+
+def isvalid(s):
+    while "()" in s or "{}" in s or "[]" in s :
+        s = s.replace("[]", "").replace("()", "").replace("{}", "")
+    return s == ""
+
+print(isvalid('()[{}]/'))
