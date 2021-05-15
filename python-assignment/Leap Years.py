089d0036a91b91e68ce6a5e349c6474db2f59ffc
Yasemin <gonenyasemin@gmail.com>
2021-05-15 04:28:57 +0300

    leap year2

--- python-assignment/Leap Years.py
+++ python-assignment/Leap Years.py
@@ -9,7 +9,17 @@
 
 
 
-    
+leap_year = int(input("Enter a year:"))
+
+if leap_year % 4 == 0 :
+    if (leap_year % 100 == 0) and (leap_year % 400 == 0):
+        print(f"{leap_year} is a leap year.")
+    elif (leap_year % 4 == 0) and (leap_year % 100 != 0):
+        print(f"{leap_year} is a leap year.")
+    else :
+        print(f"{leap_year} is not a leap year.")
+else :
+    print(f"{leap_year} is not a leap year.")
 
 
 
