8161dc2002a09b409ec14ea972816c5486e58c68
Yasemin <gonenyasemin@gmail.com>
2021-05-16 15:51:50 +0300

    workshop

---
+++ python-workshop/Morse Translator-workshop3.py
@@ -0,0 +1,67 @@
+"""
+Coding Challenge - 3: Morse Translator
+The purpose of this coding challenge is to write a program that translates the plain text to morse code.
+
+Learning Outcomes
+At the end of this coding challenge, students will be able to;
+
+Analyze a problem, identify, and apply programming knowledge for an appropriate solution.
+
+Implement loops to solve a problem.
+
+Make use of dictionary data structure to map values.
+
+Demonstrate their knowledge of algorithmic design principles by solving the problem effectively.
+
+Problem Statement
+Write a function that takes in a plain text as input and converts it into morse alphabet. Following alphabet can be used:
+
+{
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+Expected Outputs:
+Input: Hello world!
+
+Output:
+.... . .-.. .-.. ---   .-- --- .-. .-.. -.. -.-.--
+
+Input: Good job!
+
+Output: --. --- --- -..   .--- --- -... -.-.--
+"""
+mors_dict = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+
+key_str = input("Enter a word or sentences: ")
+change = key_str.upper()
+
+print(change)
+
+for i in change():
+  result += mors_dict[i]
+  print(result)
+
+print(change())
+  
+
+
+
